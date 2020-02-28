956792030e128117034a82b02ba0c58e7a4f453c
iasandoval <mail@ignaciosandoval.com>
2020-02-28 11:45:15 -0500

    Updating EncryptUtil with documentation.

--- src/main/mx/com/nacho/utils/EncryptUtil.java
+++ src/main/mx/com/nacho/utils/EncryptUtil.java
@@ -1,21 +1,20 @@
 package mx.com.nacho.utils;
 
-import javax.crypto.Cipher;
-import javax.crypto.SecretKey;
-import javax.crypto.SecretKeyFactory;
+import javax.crypto.*;
 import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.PBEKeySpec;
 import javax.crypto.spec.SecretKeySpec;
 import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
 import java.security.spec.InvalidKeySpecException;
 import java.security.spec.KeySpec;
 import java.util.Arrays;
 import java.util.Base64;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  * Utility class to encrypt and decrypt a text.
@@ -25,87 +24,157 @@ import java.util.logging.Logger;
  */
 public class EncryptUtil {
 
-    private static final Logger LOGGER = Logger.getLogger(EncryptUtil.class.getName());
-    private static final String AES_CBC_PKCS5PADDING = "AES/CBC/PKCS5Padding";
-    private static final String AES = "AES";
-    private static final String PBKDF2_WITH_HMAC_SHA1 = "PBKDF2WithHmacSHA1";
-    private static final String TEMP_KEY = "P3pePeC4sp1c4Pps";
+    private static final int BYTE_SIZE_16 = 16;
 
+    private static final String AES = "AES";
+    private static final String AES_CBC_PKCS5PADDING = "AES/CBC/PKCS5Padding";
+    private static final String PBKDF2_WITH_HMAC_SHA1 = "PBKDF2WithHmacSHA1";
+
+    private static final String DEFAULT_KEY = "P3pePeC4sp1c4Pps";
+    private static final String DEFAULT_TEXT = "nacho-utils";
+
+    /**
+     * Key used to Encrypt or Decrypt the text.
+     */
     private String key;
 
+    /**
+     * Text to Encrypt or Decrypt.
+     */
+    private String text;
+
+    /**
+     * Default constructor. It will use the Default KEY and Default TEXT.
+     */
     public EncryptUtil() {
-        this.key = TEMP_KEY;
+        this.key = DEFAULT_KEY;
+        this.text = DEFAULT_TEXT;
     }
 
-    public EncryptUtil(String key) {
-        this.key = key;
+    /**
+     * Constructor by text.
+     *
+     * @param text This is the text to Encrypt or Decrypt.
+     */
+    public EncryptUtil(String text) {
+        this.key = DEFAULT_KEY;
+        this.text = text;
     }
 
-    public String getKey()  {
+    /**
+     * Get the key used to Encrypt or Decrypt.
+     *
+     * @return The key used to Encrypt or Decrypt.
+     */
+    public String getKey() {
         return this.key;
     }
 
+    /**
+     * Set the key to use for Encrypt or Decrypt.
+     *
+     * @param key The key to use for Encrypt or Decrypt.
+     */
     public void setKey(String key) {
         this.key = key;
     }
 
+    /**
+     * Chained method to set the key to use for Encrypt or Decrypt.
+     *
+     * @param key The key to use for Encrypt or Decrypt.
+     * @return The instance of EncryptUtil.
+     */
     public EncryptUtil withKey(String key) {
         this.key = key;
         return this;
     }
 
-    public String encrypt(String value) {
-        String encrypt = null;
+    /**
+     * Chained method to Encrypt the provided text.
+     *
+     * @return Encrypted String.
+     * @throws EncryptUtilException In case of EncryptUtilException.
+     */
+    public String encrypt() throws EncryptUtilException {
+        return this.encrypt(this.text);
+    }
+
+    /**
+     * Chained method to Decrypt the provided text.
+     *
+     * @return Decryted String.
+     * @throws EncryptUtilException In case of EncryptUtilException.
+     */
+    public String decrypt() throws EncryptUtilException {
+        return this.decrypt(this.text);
+    }
+
+    /**
+     * Method to Encrypt the provided text.
+     *
+     * @param value Text to Encrypt.
+     * @return Encrypted String.
+     * @throws EncryptUtilException In case of EncryptUtilException.
+     */
+    public String encrypt(String value) throws EncryptUtilException {
         try {
-            byte[] salt = getSalt();
-            SecretKey keySpec = this.getSecretKey(salt);
-            IvParameterSpec iv = this.getIvParameterSpec();
+            byte[] salt = this.getSalt();
+            SecretKey secretKey = this.getSecretKey(salt);
+            IvParameterSpec ivParameter = this.getIvParameterSpec();
 
             Cipher cipher = Cipher.getInstance(AES_CBC_PKCS5PADDING);
-            cipher.init(Cipher.ENCRYPT_MODE, keySpec, iv);
+            cipher.init(Cipher.ENCRYPT_MODE, secretKey, ivParameter);
 
             byte[] encrypted = cipher.doFinal(value.getBytes(StandardCharsets.UTF_8));
 
             // Concatenate salt + iv + encrypted
             ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
             outputStream.write(salt);
-            outputStream.write(iv.getIV());
+            outputStream.write(ivParameter.getIV());
             outputStream.write(encrypted);
 
-            encrypt = Base64.getEncoder().encodeToString(outputStream.toByteArray());
-        } catch (Exception ex) {
-            LOGGER.log(Level.SEVERE, ex.getMessage(), ex);
+            return Base64.getEncoder().encodeToString(outputStream.toByteArray());
+
+        } catch (InvalidKeySpecException | NoSuchAlgorithmException | NoSuchPaddingException |
+                InvalidAlgorithmParameterException | InvalidKeyException | BadPaddingException |
+                IllegalBlockSizeException | IOException ex) {
+            throw new EncryptUtilException(ex.getMessage(), ex);
         }
-        return encrypt;
     }
 
-    public String decrypt(String encrypted) {
-        String decrypt = null;
+    /**
+     * Method to Decrypt the provided text.
+     *
+     * @param encrypted Encrypted text.
+     * @return Decrypted String.
+     * @throws EncryptUtilException In case of EncryptUtilException.
+     */
+    public String decrypt(String encrypted) throws EncryptUtilException {
         try {
-
             byte[] cipherText = Base64.getDecoder().decode(encrypted);
-            byte[] salt = Arrays.copyOfRange(cipherText, 0, 16);
-            byte[] iv = Arrays.copyOfRange(cipherText, 16, 32);
-            byte[] ct = Arrays.copyOfRange(cipherText, 32, cipherText.length);
+            byte[] salt = Arrays.copyOfRange(cipherText, 0, BYTE_SIZE_16);
+            byte[] ivParameter = Arrays.copyOfRange(cipherText, BYTE_SIZE_16, BYTE_SIZE_16 * 2);
+            byte[] cipherTextBytes = Arrays.copyOfRange(cipherText, BYTE_SIZE_16 * 2, cipherText.length);
 
-            SecretKey keySpec = getSecretKey(salt);
+            SecretKey keySpec = this.getSecretKey(salt);
             Cipher cipher = Cipher.getInstance(AES_CBC_PKCS5PADDING);
-            cipher.init(Cipher.DECRYPT_MODE, keySpec, new IvParameterSpec(iv));
+            cipher.init(Cipher.DECRYPT_MODE, keySpec, new IvParameterSpec(ivParameter));
 
-            byte[] original = cipher.doFinal(ct);
+            byte[] original = cipher.doFinal(cipherTextBytes);
 
-            decrypt = new String(original, StandardCharsets.UTF_8);
-        } catch (Exception ex) {
-            LOGGER.log(Level.SEVERE, ex.getMessage(), ex);
+            return new String(original, StandardCharsets.UTF_8);
+        } catch (InvalidKeySpecException | NoSuchAlgorithmException | NoSuchPaddingException |
+                InvalidAlgorithmParameterException | InvalidKeyException | BadPaddingException |
+                IllegalBlockSizeException ex) {
+            throw new EncryptUtilException(ex.getMessage(), ex);
         }
 
-        return decrypt;
     }
 
     private byte[] getSalt() {
-        SecureRandom random = new SecureRandom();
-        byte[] salt = new byte[16];
-        random.nextBytes(salt);
+        byte[] salt = new byte[BYTE_SIZE_16];
+        new SecureRandom().nextBytes(salt);
 
         return salt;
     }
@@ -120,10 +189,35 @@ public class EncryptUtil {
 
     private IvParameterSpec getIvParameterSpec() {
         SecureRandom random = new SecureRandom();
-        byte[] ivBytes = new byte[16];
+        byte[] ivBytes = new byte[BYTE_SIZE_16];
         random.nextBytes(ivBytes);
 
         return new IvParameterSpec(ivBytes);
     }
 
+    /**
+     * Wrapper Exception.
+     */
+    public class EncryptUtilException extends Exception {
+
+        public EncryptUtilException() {
+            super();
+        }
+
+        public EncryptUtilException(String message) {
+            super(message);
+        }
+
+        public EncryptUtilException(String message, Throwable cause) {
+            super(message, cause);
+        }
+
+        public EncryptUtilException(Throwable cause) {
+            super(cause);
+        }
+
+        protected EncryptUtilException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+            super(message, cause, enableSuppression, writableStackTrace);
+        }
+    }
 }
--- src/test/mx/com/nacho/utils/test/EncryptUtilTest.java
+++ src/test/mx/com/nacho/utils/test/EncryptUtilTest.java
@@ -4,15 +4,33 @@ import mx.com.nacho.utils.EncryptUtil;
 import org.junit.Test;
 
 public class EncryptUtilTest {
+
     @Test
-    public void test() {
+    public void testTraditional() {
+        try {
+            EncryptUtil enc = new EncryptUtil();
+            enc.setKey("secret");
 
-        EncryptUtil enc = new EncryptUtil().withKey("secret");
+            String encrypted = enc.encrypt("nacho");
+            String decrypted = enc.decrypt(encrypted);
 
-        String encrypted = enc.encrypt("nacho");
-        String decrypted = enc.decrypt(encrypted);
+            System.out.println(encrypted);
+            System.out.println(decrypted);
+        } catch (EncryptUtil.EncryptUtilException e) {
+            e.printStackTrace();
+        }
+    }
 
-        System.out.println(encrypted);
-        System.out.println(decrypted);
+    @Test
+    public void testChained() {
+        try {
+            String encrypted = new EncryptUtil("nacho").withKey("secret").encrypt();
+            String decrypted = new EncryptUtil(encrypted).withKey("secret").decrypt();
+
+            System.out.println(encrypted);
+            System.out.println(decrypted);
+        } catch (EncryptUtil.EncryptUtilException e) {
+            e.printStackTrace();
+        }
     }
 }
